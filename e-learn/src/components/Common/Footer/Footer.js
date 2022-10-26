7a7cea71651483510a137f2f5cf1f7c47b363d86
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-10-26 17:31:16 +0600

    header, footer and setup layout

---
+++ e-learn/src/components/Common/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-white text-center mt-5 py-5">
+            <p>Copyright ©2022 Pranto - All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ e-learn/src/components/Common/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import logo from '../../../assets/imgs/e.png'
+import './Header.css'
+const Header = () => {
+    return (
+        <Navbar bg="light" variant="light" expand="lg" className='p-3 rounded'>
+            <Container>
+                <Navbar.Brand href="/" className='fw-bold logo'><img src={logo} alt='Logo' width={30} /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <LinkContainer to="/">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="topics">
+                            <Nav.Link>Quiz Topics</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="statistics">
+                            <Nav.Link>Statistics</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="blog">
+                            <Nav.Link>Blog</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
