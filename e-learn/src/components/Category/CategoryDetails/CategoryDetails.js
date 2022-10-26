09588662a0bfc165493f12ebb863b8dea63eaaee
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-10-27 03:53:35 +0600

    used react to pdf package

--- e-learn/src/components/Category/CategoryDetails/CategoryDetails.js
+++ e-learn/src/components/Category/CategoryDetails/CategoryDetails.js
@@ -3,14 +3,21 @@ import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 import { FaCartPlus, FaCloudDownloadAlt } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
 const CategoryDetails = () => {
     const cDetails = useLoaderData(); //cDetails = category details
     const { _id, price, course_name, course_duration, description, topics, picture, course_start } = cDetails;
+    const options = {
+        orientation: 'landscape',
+        unit: 'in',
+        format: [4, 2]
+    };
     return (
         <Container>
             <Row>
-                <Col md={12} className="my-5">
-                    <Card className='shadow bg-body rounded border-0 text-secondary '>
+                <Col md={12} className="my-5" ref={ref} options={options} x={.5} y={.5} scale={0.8}>
+                    <Card className='shadow bg-body rounded border-0 text-secondary'>
                         <Card.Header className='border-0 bg-light' as="h3">{course_name}</Card.Header>
                         <Card.Body>
                             <Row>
@@ -35,7 +42,13 @@ const CategoryDetails = () => {
                                     <h2 className='text-danger fw-bold'>${price}</h2>
                                     <p className='fw-bold my-3'>Course Duration: <span className='text-dark'>{course_duration} months</span></p>
                                     <p className='fw-bold my-3'>Course Will Start: <span className='text-dark'> {course_start}</span></p>
-                                    <Button variant='outline-danger' className='animate__animated animate__bounceInDown my-2'>Download Course details <FaCloudDownloadAlt /> </Button>
+
+                                    <Pdf targetRef={ref} filename={`${course_name}.pdf`}>
+                                        {({ toPdf }) => <Button variant='outline-danger' className='animate__animated animate__bounceInDown my-2' onClick={toPdf}>Download Course details <FaCloudDownloadAlt /></Button>}
+                                    </Pdf>
+
+
+
                                     <Button className='animate__animated animate__bounceInDown' variant="outline-primary"><Link to={`/checkout/${_id}`} className='nav-link'>Get Premium <FaCartPlus /> </Link></Button>
                                 </Col>
                             </Row>
