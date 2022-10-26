b3ad2f9cedb55b9a8033696dd13158707bbff68b
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-10-27 00:24:42 +0600

    checkout page word done

--- e-learn/src/components/CheckOut/CheckOut.js
+++ e-learn/src/components/CheckOut/CheckOut.js
@@ -1,51 +1,79 @@
-import React, { useState } from 'react';
-import { Badge, Button, Card, Col, Container, Modal, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Badge, Button, Card, Col, Container, Modal, Row, Spinner } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 import './CheckOut.css'
 import paypal from '../../assets/imgs/paypal.png'
 import payoneer from '../../assets/imgs/symbols.png'
 import visa from '../../assets/imgs/visa.png'
+import paymentGif from '../../assets/imgs/payment-complete.gif'
+import { success } from '../../Toasts/Toasts';
 const CheckOut = () => {
     const cBuy = useLoaderData(); //cBuy = course buy
     const { price, course_name, topics, picture, description } = cBuy;
     const [show, setShow] = useState(false);
-
+    const [loading, setLoading] = useState(false);
+    const [coursePayment, setCoursePayment] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const payment = () => {
+        setLoading(true)
+    }
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (loading === true) {
+                setCoursePayment(true)
+                success('Payment Completed!')
+            }
+            setLoading(false);
+            setShow(false)
+
+        }, 5000);
+        return () => clearTimeout(timer);
+    }, [loading]);
     return (
         <Container>
             <Row>
                 <Col md={12} className="my-5">
                     <Card className="shadow-lg p-3 mb-5 bg-body rounded border-0">
                         <Card.Body>
-                            <Row>
-                                <Col md={8}>
-                                    <div className='d-flex'>
-                                        <Card.Img variant="top" src={picture} className="img-fluid img shadow-lg rounded" />
-                                        <div>
-                                            <Card.Title className='p-2 text-secondary'>
-                                                {course_name}
+                            {
+                                coursePayment === false ?
+                                    <Row>
+                                        <Col md={8}>
+                                            <div className='d-flex'>
+                                                <Card.Img variant="top" src={picture} className="img-fluid img shadow-lg rounded" />
+                                                <div>
+                                                    <Card.Title className='p-2 text-secondary'>
+                                                        {course_name}
+                                                    </Card.Title>
+                                                    {topics.map((t, i) => <Badge key={i} className="mx-2" bg="dark" text="light"> {t} </Badge>)}
+                                                    <Card.Text className='text-secondary ms-2 mt-2'>
+                                                        {
+                                                            description.length > 100
+                                                                ?
+                                                                description.slice(0, 100)
+                                                                :
+                                                                description
+                                                        }
+                                                    </Card.Text>
+                                                </div>
+                                            </div>
+                                        </Col>
+                                        <Col md={4} className="text-end">
+                                            <Card.Title className='p-2 text-danger fw-bold' as="h1">
+                                                ${price}
                                             </Card.Title>
-                                            {topics.map((t, i) => <Badge key={i} className="mx-2" bg="dark" text="light"> {t} </Badge>)}
-                                            <Card.Text className='text-secondary ms-2 mt-2'>
-                                                {
-                                                    description.length > 100
-                                                        ?
-                                                        description.slice(0, 100)
-                                                        :
-                                                        description
-                                                }
-                                            </Card.Text>
-                                        </div>
-                                    </div>
-                                </Col>
-                                <Col md={4} className="text-end">
-                                    <Card.Title className='p-2 text-danger fw-bold' as="h1">
-                                        ${price}
+                                            <Button onClick={handleShow}>Proceed To Checkout</Button>
+                                        </Col>
+                                    </Row>
+                                    :
+
+                                    <Card.Title className='text-center' as="h1">
+                                        <h1 className='text-success'>PAID</h1>
+                                        <img className='img-fluid' src={paymentGif} alt="" />
                                     </Card.Title>
-                                    <Button onClick={handleShow}>Proceed To Checkout</Button>
-                                </Col>
-                            </Row>
+
+                            }
                         </Card.Body>
                     </Card>
                 </Col>
@@ -56,15 +84,24 @@ const CheckOut = () => {
                 </Modal.Header>
                 <Modal.Body>
                     <Row className='d-flex justify-content-between'>
-                        <Col md={3} role="button">
-                            <Card.Img variant="top" src={paypal} className="img-fluid" />
-                        </Col>
-                        <Col md={3} role="button">
-                            <Card.Img variant="top" src={payoneer} className="img-fluid" />
-                        </Col>
-                        <Col md={3} role="button">
-                            <Card.Img variant="top" src={visa} className="img-fluid" />
-                        </Col>
+                        {
+                            loading === false
+                                ?
+                                <>
+                                    <Col md={3} role="button" onClick={payment}>
+                                        <Card.Img variant="top" src={paypal} className="img-fluid" />
+                                    </Col>
+                                    <Col md={3} role="button" onClick={payment}>
+                                        <Card.Img variant="top" src={payoneer} className="img-fluid" />
+                                    </Col>
+                                    <Col md={3} role="button" onClick={payment}>
+                                        <Card.Img variant="top" src={visa} className="img-fluid" />
+                                    </Col>
+                                </>
+                                :
+                                <Spinner className='mx-auto' animation="grow" />
+                        }
+
                     </Row>
 
                 </Modal.Body>
